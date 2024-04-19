2ed5e1460e2c39f9631be7e3cd65c42406725095
Nikita Khorev <codenrock.nk@gmail.com>
2024-04-19 19:02:18 +0300

    контейнеризиировал

--- backendWS/src/routes/index.ts
+++ backendWS/src/routes/index.ts
@@ -1,8 +1,11 @@
-import { Router } from 'express';
-import { postWord } from '../controllers/word-controller'
+import {Router, Request, Response} from 'express';
+import {postWord} from '../controllers/word-controller'
 
 const router = Router();
 
+router.get('/', (req: Request, res: Response) => {
+    res.status(200).send('health check');
+});
 router.post('/word', postWord);
 
 export default router;
