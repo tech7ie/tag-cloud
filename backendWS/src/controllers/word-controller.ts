0d8b47a3c72f2f4d14fa69f65d326cd2b7e82224
nikita <nikita.ulianov2013@yandex.ru>
2024-06-15 13:39:08 +0500

    db added

--- backendWS/src/controllers/word-controller.ts
+++ backendWS/src/controllers/word-controller.ts
@@ -1,43 +1,28 @@
 import { Request, Response } from 'express';
-import OpenAI from 'openai';
-import { broadcast } from '../websockets/word-websoket'
+import { broadcast } from '../websockets/word-websoket';
+import pool from '../database/db';
 import { config } from 'dotenv';
 config();
 
-
 export async function postWord(req: Request, res: Response) {
-    try {
-      let word = req.body.word.toLowerCase();
-      (async (str) => {
-        const regex = /^[a-zA-Zа-яА-ЯёЁ]{1,15}$/;
-        if (regex.test(str)) {
-            let boolStr = await sendPrompt(str);
-            console.log(boolStr);
-            if (boolStr === '0') {
-                broadcast(str);
-            }
-        }
-      })(word);
-      res.send('sent');
-    } catch (error) {
-      console.error(error);
-      res.status(500).send('An error occurred');
+  try {
+    const client = await pool.connect();
+    let word = req.body.word.toLowerCase();
+    const regex = /^[a-zA-Zа-яА-ЯёЁ]{1,15}$/;
+    
+    if (regex.test(word)) {
+      try {
+        await client.query('INSERT INTO words (word) VALUES ($1)', [word]);
+        broadcast(word);
+        res.send('sent');
+      } finally {
+        client.release();
+      }
+    } else {
+      res.status(400).send('Invalid word format');
     }
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('An error occurred');
   }
-
-
-const openai = new OpenAI({
-    apiKey: process.env.OPENAI_API_KEY as string,
-  });
-
-async function sendPrompt(word: string) {
-  const chatCompletion = await openai.chat.completions.create({
-    messages: [{
-        role: 'user',
-        content: `если строка "${word}" является словом и словом оскорбительным или нецензурным то
-                     ОТПРАВЬ ТОЛЬКО ЦИФРУ: если да, то 1, если нет - 0 `
-        }],
-    model: 'gpt-4',
-  });
-  return chatCompletion.choices[0].message.content
 }
